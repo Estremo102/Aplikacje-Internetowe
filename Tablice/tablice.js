e923e24311be5d413469f59455d8b58cbcf813f2
Dominik Możdżeń <dominikmozdzen@gmail.com>
2025-10-06 13:19:26 +0200

    funkcje sort i compare

--- Tablice/tablice.js
+++ Tablice/tablice.js
@@ -22,7 +22,53 @@ function InsertionSort(array){
     }
 }
 
-//Zadanie:
 function BubbleSort(array){
-    //Zaimplementuj sortowanie bąbelkowe
+    for(let i = 0; i < array.length - 1; i++){
+        for(let j = 0; j < array.length - 1 - i; j++){
+            if(array[j] > array[j+1])
+            {
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+}
+
+function compare(a, b){
+    if(a < b){
+        return -1;
+    }
+    if(a > b){
+        return 1;
+    }
+    return 0;
+}
+
+function compare2(a, b){
+    return a < b ? -1 : a > b ? 1 : 0;
+}
+
+function compare3(a, b){
+    return a - b;
+}
+
+tablica.sort((a, b) => a - b);
+
+function compareString(a, b){
+    return a.localeCompare(b, "pl", {sinsitivity : "base"});
+}
+
+function compareModulo(a, b){
+    if(a%2 == b%2){
+        if(a%2==0)
+        {
+            return a-b;
+        }
+        return b-a;
+    }
+    if(a%2==0){
+        return 1;
+    }
+    return -1;
 }
