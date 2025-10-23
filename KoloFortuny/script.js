2b8bd3041354c60aca2d7dc8a4789a651de41f20
Dominik Możdżeń <dominikmozdzen@gmail.com>
2025-10-23 10:42:50 +0200

    class uczen, wylosowanyUczen

--- KoloFortuny/script.js
+++ KoloFortuny/script.js
@@ -6,13 +6,29 @@
 
 let prompt = window.prompt("Wprowadź uczniów oddzielonych za pomocą średników")
 const uczniowie = prompt.split(';');
-let random = Math.floor(Math.random()*uczniowie.length);
 const lista = document.getElementById('lista');
-for(let i = 0; i < uczniowie.length; i++){
-    if(i==random){
-        lista.innerHTML += `<li style='color: #F00>${uczniowie[i]}</li>`;
-    }else{
-        lista.innerHTML += `<li>${uczniowie[i]}</li>`;
+Wyswielt();
+
+function Wyswielt()
+{
+    lista.innerHTML = "";
+    for(let i = 0; i < uczniowie.length; i++){
+        // if(i==random){
+        //     lista.innerHTML += `<li><div class="wylosowanyUczen">${uczniowie[i]}</div></li>`;
+        // }else{
+            lista.innerHTML += `<li><div class="uczen">${uczniowie[i]}</div></li>`;
+        // }
+    }
+}
+
+function Losuj(){
+    Wyswielt();
+    let uczniowie = document.getElementsByClassName("uczen");
+    let random = Math.floor(Math.random()*uczniowie.length);
+    for(let i = 0; i < uczniowie.length; i++){
+        if(i==random){
+            uczniowie[i].className = "wylosowanyUczen";
+        }
     }
 }
 
