3f928b0ece89ae547946ce33045486dba0efcd32
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2025-11-02 22:45:34 +0100

    Zadanie 3

--- Funkcje/script.js
+++ Funkcje/script.js
@@ -9,6 +9,11 @@ try{
                     window.odejmij.toString();
 }catch{}
 
+try{
+    let z3s = document.querySelector('#z3s');
+    z3s.innerHTML = window.mnoz.toString();
+}catch{}
+
 let code = document.querySelectorAll('pre code');
 code.forEach((block) => {
     block.innerHTML = block.textContent.replace(/return/g, '<span class="pink">return</span>')
@@ -17,7 +22,8 @@ code.forEach((block) => {
     .replace(/function\s+([a-zA-Z_$][0-9a-zA-Z_$]*)/g, 'function <span class="blue">$1</span>')
     .replace(/function/g, '<span class="yellow">function</span>')
     .replace(/Math./g, '<span class="lightblue">Math</span>.')
-    .replace('.pow', '.<span class="yellow">pow</span>');
+    .replace('.pow', '.<span class="yellow">pow</span>')
+    .replace(/for/g, '<span class="pink">for</span>');
 });
 
 window.onload = function() {
@@ -39,6 +45,18 @@ window.onload = function() {
                 if (window.dodaj(2,3) !== 5) return false;
                 if (typeof window.odejmij !== 'function') return false;
                 if (window.odejmij(5,2) !== 3) return false;
+                return true;
+            } catch { return false; }
+        },
+        // Zadanie 3
+        () => {
+            try {
+                if (!window.mnoz) return false;
+                if (typeof window.mnoz !== 'function') return false;
+                if (window.mnoz(2,3) !== 6) return false;
+                if (window.mnoz(2,3,4) !== 24) return false;
+                if (window.mnoz(1,2,3,4,5) !== 120) return false;
+                if (window.mnoz() !== 1 ) return false;
                 return true;
             } catch { return false; }
         }
---
+++ Funkcje/zadanie3.js
@@ -0,0 +1,6 @@
+// Tutaj napisz swoje rozwiązanie
+
+// Kod do testów, nie usuwać
+try{
+    window.mnoz = mnoz;
+}catch{}
