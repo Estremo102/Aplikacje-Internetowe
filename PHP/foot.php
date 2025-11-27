3f4026cb44f68adf75a8e2ad0127a509a85ddea7
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2025-11-27 21:41:09 +0100

    github logo in footer

--- PHP/foot.php
+++ PHP/foot.php
@@ -1,4 +1,7 @@
     <footer>
+        <a href="https://github.com/estremo102" height="80">
+            <img src="/php/images/github.svg" alt="Github">
+        </a>
         <p>&copy; 2025</p>
     </footer>
 </body>
--- PHP/head.php
+++ PHP/head.php
@@ -5,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kurs PHP</title>
     <link rel="stylesheet" href="/php/style.css">
-    <?php ?>
     <script src="script.js" defer></script>
 </head>
 <body>
     <header>
-        <img src="/php/PHP-logo.svg"><h1> Kurs PHP - <?php echo ucfirst(explode('.', basename($_SERVER['PHP_SELF']))[0]); ?></h1>
+        <img src="/php/images/PHP-logo.svg"><h1> Kurs PHP - 
+            <?php 
+            $title = ucfirst(explode('.', basename($_SERVER['PHP_SELF']))[0]);
+            echo $title == 'Index' ? 'Strona Główna' : $title;
+            ?>
+        </h1>
     </header>
