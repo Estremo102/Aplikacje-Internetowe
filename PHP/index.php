0082d446f51d86a44f31b8de8b3abb319b9085bb
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2025-11-27 22:24:37 +0100

    style update

--- PHP/index.php
+++ PHP/index.php
@@ -1,10 +1,14 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/php/head.php'; ?>
 <div class="content">
     <aside>
-        
+        <ul>
+            <a href="/php/wprowadzenie/wprowadzenie.php"><li>Wprowadzenie</li></a>
+        </ul>
     </aside>
     <main>
-        
+        <section>
+            <p>Witaj w kursie programowania w języku PHP.</p>
+        </section>
     </main>
 </div>
 <?php include $_SERVER['DOCUMENT_ROOT'].'/php/foot.php'; ?>
