b45c108ce5af6454e16b5b83e72bf35474cee6e9
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2025-12-10 20:36:21 +0100

    dodanie podstrony polaczenie z baza danych

---
+++ PHP/polaczenie_z_baza_danych/polaczenie_z_baza_danych.php
@@ -0,0 +1,69 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/php/head.php'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/php/navbox.php'; ?>
+<div class="content">
+    <aside>
+        <ul>
+
+        </ul>
+    </aside>
+    <main>
+        <section id="zadanie1">
+            <h2>Zadanie 1</h2>
+            <h3>Rodzaje typów danych</h3>
+            
+            <div class="solution-container">
+                <?php include 'testy/zadanie1_test.php'; ?>
+            </div>
+        </section>
+        
+        <section id="zadanie2">
+            <h2>Zadanie 2</h2>
+            <h3>Ciągi znaków</h3>
+            
+            <div class="solution-container">
+            <?php include 'testy/zadanie2_test.php'; ?>
+            </div>
+        </section>
+        
+        <section id="zadanie3">
+            <h2>Zadanie 3</h2>
+            <h3>Edycja ciągów znaków</h3>
+            
+            <div class="solution-container">
+
+            <?php include 'testy/zadanie3_test.php'; ?>
+            </div>
+        </section>
+        
+        <section id="zadanie4">
+            <h2>Zadanie 4</h2>
+            <h3>łączenie i wydzielanie ciągów znaków</h3>
+            
+            <div class="solution-container">
+
+            <?php include 'testy/zadanie4_test.php'; ?>
+            </div>
+        </section>
+        
+        <section id="zadanie5">
+            <h2>Zadanie 5</h2>
+            <h3>Znak ucieczki</h3>
+            
+            <div class="solution-container">
+                
+                <?php include 'testy/zadanie5_test.php'; ?>
+            </div>
+        </section>
+        
+        <section id="zadanie6">
+            <h2>Zadanie 6</h2>
+            <h3>Liczby</h3>
+
+            <div class="solution-container">
+                
+                <?php include 'testy/zadanie6_test.php'; ?>
+            </div>
+        </section>
+    </main>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/php/foot.php'; ?>
