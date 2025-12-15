34b9400fe6931c2aa0a5e719dde1d4a1f0687c82
Dominik Możdżeń <dominikmozdzen@gmail.com>
2025-12-15 13:21:15 +0100

    saper

--- Tablice/tablice.js
+++ Tablice/tablice.js
@@ -72,3 +72,32 @@ function compareModulo(a, b){
     }
     return -1;
 }
+
+function Saper(arr){
+    if(!Array.isArray(arr)) return false;
+    if(!arr.every(el => Array.isArray(el))) return false;
+    const result = [];
+    for(let i = 0; i < arr.length; i++){
+        result[i] = [];
+        result[i].length = arr[i].length;
+        result[i].fill(0);
+    }
+
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr[i].length; j++){
+            if(arr[i][j]){
+                result[i][j] = 'X';
+                for(let a = -1; a < 2; a++){
+                    for(let b = -1; b < 2; b++){
+                        try{
+                            if(typeof result[i + a][j + b] === "number"){
+                                result[i + a][j + b]++;
+                            }
+                        } catch{}
+                    }
+                }
+            }
+        }
+    }
+    return result;
+}
