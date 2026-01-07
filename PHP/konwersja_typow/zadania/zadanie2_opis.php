8329ae706a1e8dd74c5aa3b34842abeb0471a1b6
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-01-07 10:10:36 +0100

    rzutowanie zad2 teoria

--- PHP/konwersja_typow/zadania/zadanie2_opis.php
+++ PHP/konwersja_typow/zadania/zadanie2_opis.php
@@ -1,2 +1,16 @@
-<h3>Tytul zadania</h3>
-<p>tresc zadania Lorem ipsum dolor sit amet consectetur adipisicing elit. A dignissimos distinctio explicabo, assumenda accusantium quod obcaecati laborum, nam odio aspernatur natus ratione tempore nisi ab odit minima saepe et alias!</p>
+<h3>Rzutowanie typu PHP</h3>
+<p>
+Rzutowanie typu PHP to jawny proces konwersji wartości z jednego typu danych na inny, na przykład z float na integer.
+Daje to programiście bezpośrednią kontrolę nad typem danych zmiennej.
+Rzutowanie zmiennej odbywa się poprzez umieszczenie typu rzutowania w nawiasach przed zmienną lub wartością, którą chcesz przekonwertować.
+</p>
+<ul>
+    Operatory rzutowania PHP to:
+    <li>(string) - Konwertuje na typ danych String</li>
+    <li>(int) - Konwertuje na typ danych Integer</li>
+    <li>(float) - Konwertuje na typ danych Float</li>
+    <li>(bool) - Konwertuje na typ danych Boolean</li>
+    <li>(array) - Konwertuje na typ danych Array</li>
+    <li>(object) - Konwertuje na typ danych Object</li>
+    <li>(unset) - Przestarzałe. Konwertuje na typ danych NULL</li>
+</ul>
