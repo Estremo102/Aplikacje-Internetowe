1653987267205569726d0825a431994b99ca3d19
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-01-14 13:11:37 +0100

    Zadanie 4

--- PHP/konwersja_typow/zadania/zadanie4_opis.php
+++ PHP/konwersja_typow/zadania/zadanie4_opis.php
@@ -1,2 +1,15 @@
-<h3>Tytul zadania</h3>
-<p>tresc zadania Lorem ipsum dolor sit amet consectetur adipisicing elit. A dignissimos distinctio explicabo, assumenda accusantium quod obcaecati laborum, nam odio aspernatur natus ratione tempore nisi ab odit minima saepe et alias!</p>
+<h3>Rzutowanie do Boolean</h3>
+Dany jest poniższy kod PHP:
+<pre><code class="language-php">&lt;?php
+$aa = 5;       // Integer
+$bb = 5.34;    // Float
+$cc = 0;       // Integer
+$dd = -1;      // Integer
+$ee = 0.1;     // Float
+$ff = "hello"; // String
+$gg = "";      // String
+$hh = true;    // Boolean
+$ii = NULL;    // NULL
+?&gt;</code></pre>
+<p> zrzutuj dane do typu Boolean i wyświetl wartości zmiennych <code>$aa</code>, <code>$bb</code>, <code>$cc</code>, <code>$dd</code>, <code>$ee</code>, <code>$ff</code>, <code>$gg</code> i <code>$ii</code> po konwersji do typu Boolean.</p>
+<p>Użyj funkcji <code>var_dump()</code>, aby wyświetlić zarówno typ, jak i wartość każdej zmiennej po konwersji. Następnie przeanalizuj wyniki.</p>
