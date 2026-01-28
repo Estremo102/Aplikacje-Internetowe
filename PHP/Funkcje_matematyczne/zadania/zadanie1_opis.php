4add0f26e479627b93796b1b732e5f0973a64ae7
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-01-28 13:17:30 +0100

    treść zadania 1

--- PHP/Funkcje_matematyczne/zadania/zadanie1_opis.php
+++ PHP/Funkcje_matematyczne/zadania/zadanie1_opis.php
@@ -2,3 +2,11 @@
 <p>Funkcja pi() zwraca wartość PI:</p>
 <pre><code class="language-php">echo(pi());</code></pre>
 <pre><code><?php echo(pi()); ?></code></pre>
+<p>
+    Twoim zadaniem jest policzenie i wyświetlenie obwodu koła. Napisz skrypt w którym:
+    <ul>
+        <li>utworzysz zmienną $r lub $promień i przypiszesz do niej wartość 5</li>
+        <li>utworzysz zmienną $pole_kola i zapiszesz w nim  wynik wzoru &pi;r<sup>2</sup></li>
+        <li>wyświetlisz napis: 'pole koła o promieniu 5 wynosi {$pole_kola}'</li>
+    </ul>
+</p>
--- PHP/Funkcje_matematyczne/zadania/zadanie2_opis.php
+++ PHP/Funkcje_matematyczne/zadania/zadanie2_opis.php
@@ -1,6 +1,6 @@
 <h3>Funkcje min() i max()</h3>
 <p>Funkcje min() i max() zwracają najmniejszą lub największą wartość z podanych argumentów</p>
-<pre><code class="language-php">echo(min(0, 150, -8, -200));
+<pre><code class="language-php">echo(min(0, 150, -8, -200) . '&lt;br&gt;');
 echo(max(0, 150, -8, -200));</code></pre>
-<pre><code><?php echo(min(0, 150, -8, -200));
+<pre><code><?php echo(min(0, 150, -8, -200) . '<br>');
 echo(max(0, 150, -8, -200)); ?></code></pre>
