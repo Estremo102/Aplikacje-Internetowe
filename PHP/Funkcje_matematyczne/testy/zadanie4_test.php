17a55496597709d03f4276919950e14562b76ed4
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-02-15 23:53:58 +0100

    Zadanie 4

--- PHP/Funkcje_matematyczne/testy/zadanie4_test.php
+++ PHP/Funkcje_matematyczne/testy/zadanie4_test.php
@@ -1,4 +1,21 @@
 <?php
+    $a = 3;
+    $b = 4;
     include __DIR__ . '/../rozwiazania/zadanie4.php';
-    echo "<Script>progress[3] = 0;</Script>"; 
+    if(isset($c)) {
+        if($c == 5) {
+            echo 'Dla trójkąta a = 3, b = 4 podano prawidłowy wynik c = ' . $c; 
+            $a = 8;
+            $b = 15;
+            include __DIR__ . '/../rozwiazania/zadanie4.php';
+            if($c == 17) {
+                echo '<br>Dla trójkąta a = 8, b = 15 podano prawidłowy wynik c = ' . $c; 
+                echo "<Script>progress[3] = 1;</Script>"; 
+            } else {
+                echo '<br>Dla trójkąta a = 8, b = 15 podano nieprawidłowy wynik c = ' . $c; 
+            }
+        } else {
+            echo 'Dla trójkąta a = 3, b = 4 podano nieprawidłowy wynik c = ' . $c; 
+        }
+    }
 ?>
