317ad514ebbfcd4b9dabd05498fac49f1d226d55
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-02-15 22:48:12 +0100

    funkcje matematyczne zadanie 1 test

--- PHP/Funkcje_matematyczne/testy/zadanie1_test.php
+++ PHP/Funkcje_matematyczne/testy/zadanie1_test.php
@@ -1,5 +1,9 @@
  <?php
     echo '<script>let progress = [0, 0, 0, 0, 0, 0];</script>';
     include __DIR__ . '/../rozwiazania/zadanie1.php';
-    echo "<Script>progress[0] = 1;</Script>"; 
+    echo '<Script>
+        if(document.querySelector("#zadanie1 .solution-container").innerText === "pole koła o promieniu 5 wynosi 78.539816339745") {
+            progress[0] = 1;
+        }
+    </Script>'; 
 ?> 
--- PHP/Funkcje_matematyczne/zadania/zadanie1_opis.php
+++ PHP/Funkcje_matematyczne/zadania/zadanie1_opis.php
@@ -5,8 +5,8 @@
 <p>
     Twoim zadaniem jest policzenie i wyświetlenie obwodu koła. Napisz skrypt w którym:
     <ul>
-        <li>utworzysz zmienną $r lub $promień i przypiszesz do niej wartość 5</li>
+        <li>utworzysz zmienną $r i przypiszesz do niej wartość 5</li>
         <li>utworzysz zmienną $pole_kola i zapiszesz w nim  wynik wzoru &pi;r<sup>2</sup></li>
-        <li>wyświetlisz napis: 'pole koła o promieniu 5 wynosi {$pole_kola}'</li>
+        <li>wyświetlisz napis: "pole koła o promieniu 5 wynosi {$pole_kola}"</li>
     </ul>
 </p>
