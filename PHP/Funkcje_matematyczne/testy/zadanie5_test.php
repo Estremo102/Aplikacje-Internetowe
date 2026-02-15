e02dea6cbc70fbae7a7ed55e0b8d1b990a8d8201
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-02-16 00:22:36 +0100

    zadanie 5

--- PHP/Funkcje_matematyczne/testy/zadanie5_test.php
+++ PHP/Funkcje_matematyczne/testy/zadanie5_test.php
@@ -1,4 +1,11 @@
 <?php
+  $liczba = 21.37;
   include __DIR__ . '/../rozwiazania/zadanie5.php';
-  echo "<Script>progress[4] = 0;</Script>"; 
+  echo '<br>';
+  $liczba = 6.7;
+  include __DIR__ . '/../rozwiazania/zadanie5.php';
+  echo '<Script> if(document.querySelector("#zadanie5 .solution-container").innerText.replaceAll("\\n", " ") === "Liczba 21.37 jest zaokrąglana w dół Liczba 6.7 jest zaokrąglana w górę") {
+    progress[4] = 1;
+  }
+  </Script>'; 
 ?>
--- PHP/Funkcje_matematyczne/zadania/zadanie5_opis.php
+++ PHP/Funkcje_matematyczne/zadania/zadanie5_opis.php
@@ -4,3 +4,7 @@
 echo(round(0.49));</code></pre>
 <pre><code><?php echo(round(3.60));
 echo(round(0.49)); ?></code></pre>
+<p>
+    Twoim zadaniem jest sprawdzić, czy wartość ze zmiennej $liczba powinna być zaokrąglona w górę czy w dół. Wykorzystaj intval($liczba) lub rzutowanie (int). <br>
+    Jeśli tak, wyświetl napis "Liczba {$liczba} jest zaokrąglana w dół", w przeciwnym wypadku wyświetl napis "Liczba {$liczba} jest zaokrąglana w górę";
+</p>
