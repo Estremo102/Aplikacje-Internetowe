7c33d836329b349d8837074f6509f8e8637e6bf5
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-02-15 23:38:08 +0100

    zadanie 3

--- PHP/Funkcje_matematyczne/testy/zadanie3_test.php
+++ PHP/Funkcje_matematyczne/testy/zadanie3_test.php
@@ -1,4 +1,24 @@
 <?php
+    $user_a = 76;
+    $user_b = 9;
     include __DIR__ . '/../rozwiazania/zadanie3.php';
-    echo "<Script>progress[2] = 0;</Script>"; 
+    if(isset($roznica_wieku)){
+        echo $roznica_wieku;
+        if(abs($user_a - $user_b) == $roznica_wieku) {
+            $user_a = 21;
+            $user_b = 37;
+            include __DIR__ . '/../rozwiazania/zadanie3.php';
+            if(abs($user_a - $user_b) != $roznica_wieku) {
+                echo 'funkcja abs() nie została użyta';
+            }
+        } else {
+            echo 'Wynik niepoprawny, upewnij się, że funkcja abs() została użyta';
+        }
+    } else {
+        echo 'Nie utworzono zmiennej $roznica_wieku';
+    }; 
+    echo '<Script> if(document.querySelector("#zadanie3 .solution-container").innerText === "67") {
+        progress[2] = 1;
+    }
+    </Script>';
 ?>
--- PHP/Funkcje_matematyczne/zadania/zadanie3_opis.php
+++ PHP/Funkcje_matematyczne/zadania/zadanie3_opis.php
@@ -2,3 +2,4 @@
 <p>Funkcja abs() zwraca wartość bezwzględną</p>
 <pre><code class="language-php">echo(abs(-6.7));</code></pre>
 <pre><code><?php echo(abs(-6.7)); ?></code></pre>
+<p>Dane są dwie zmienne $user_a i $user_b. Przypisane do nich wartości to wiek użytkowników. Twoim zadaniem jest obliczenie różnicy wieku między nimi, przypisanie wyniku do zmiennej $roznica_wieku.</p>
