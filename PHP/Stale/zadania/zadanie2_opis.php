66edf13d64f370bbb467a234ab380bb6aba90707
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-02-23 08:21:00 +0100

    stale hotfix 2

--- PHP/Stale/zadania/zadanie2_opis.php
+++ PHP/Stale/zadania/zadanie2_opis.php
@@ -1,7 +1,7 @@
 <h3>Słowo kluczowe const w PHP</h3>
 <p>Słowo kluczowe const definiuje stałą w czasie kompilacji.</p>
 <p>Stałe zdefiniowane za pomocą słowa kluczowego const muszą być deklarowane w zakresie najwyższego poziomu, ponieważ są definiowane w czasie kompilacji. Oznacza to, że nie można ich deklarować wewnątrz funkcji, pętli, instrukcji if/else ani bloków try/catch. Stałe zdefiniowane za pomocą słowa kluczowego const mogą być używane do definiowania stałych w klasach. Stałe zdefiniowane za pomocą słowa kluczowego const rozróżniają wielkość liter.</p>
-<p>Składnia const <pre><code class="language-php">CONSTANT_NAME = value;</code></pre></p>
+<p>Składnia const <pre><code class="language-php">const CONSTANT_NAME = value;</code></pre></p>
 <p>W poniższym kodzie utwórz stałe na oba poznane sposoby w wyznaczonych miejscach
 <pre><code class="language-php"> // Utwórz tutaj stałą NAME i przypisz jej swoje imię.
     
