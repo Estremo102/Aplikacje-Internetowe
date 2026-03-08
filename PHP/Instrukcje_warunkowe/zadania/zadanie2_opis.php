2d6940dcfe253124369cd9955ff9d3b44316026d
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-03-08 20:08:57 +0100

    instrukcje warunkowe zadanie 2 teoria

--- PHP/Instrukcje_warunkowe/zadania/zadanie2_opis.php
+++ PHP/Instrukcje_warunkowe/zadania/zadanie2_opis.php
@@ -1,2 +1,20 @@
 <h3>instrukcja if</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid pariatur porro dicta perferendis quibusdam soluta recusandae. Accusantium ducimus perspiciatis velit, vitae tempore ipsum, cum incidunt illum exercitationem fuga repellendus doloribus.</p>
+<p>
+    <h4>Instrukcje warunkowe</h4>
+    Instrukcje warunkowe służą do wykonywania różnych akcji w oparciu o różne warunki. W PHP mamy następujące instrukcje warunkowe:
+    <ul>
+        <li>Instrukcja if – wykonuje pewien kod, jeśli jeden z warunków jest spełniony</li>
+        <li>Instrukcja if...else – wykonuje pewien kod, jeśli warunek jest spełniony, a inny kod, jeśli ten warunek jest fałszywy</li>
+        <li>Instrukcja if...elseif...else – wykonuje różne kody dla więcej niż dwóch warunków</li>
+        <li>Instrukcja switch – wybiera jeden z wielu bloków kodu do wykonania</li>
+    </ul>
+</p>
+<p>
+    <h4>Instrukcja if</h4>
+    if wykonuje kod tylko jeśli warunek logiczny jest prawdziwy
+    <pre><code class="language-php">if (warunek_logiczny) {
+        // kod który wykona się jeśli warunek logiczny jest prawdziwy
+    }</code></pre>
+    przykład
+</p>
+<p>Zadanie</p>
