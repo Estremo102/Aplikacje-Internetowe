8a694c574dfdf1e681de615c8080cf6227b45633
Estremo102 <56071387+Estremo102@users.noreply.github.com>
2026-03-08 21:57:51 +0100

    Instrukcje warunkowe Zadanie 5

--- PHP/Instrukcje_warunkowe/zadania/zadanie5_opis.php
+++ PHP/Instrukcje_warunkowe/zadania/zadanie5_opis.php
@@ -1,2 +1,29 @@
-<h3>Opis zadania 5</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid pariatur porro dicta perferendis quibusdam soluta recusandae. Accusantium ducimus perspiciatis velit, vitae tempore ipsum, cum incidunt illum exercitationem fuga repellendus doloribus.</p>
+<h3>Skrócony zapis</h3>
+<p>Jeśli instrukcja warunkowa wykonuje tylko jedną akcje, można pominąć {} <br> przykład:</p>
+<pre><code class="language-php">$a = 5;
+
+if ($a < 10) $b = "Hello";
+
+echo $b;</code></pre>
+<p>Do skrócenia instrukcji if...else można użyć poznanego w pierwszym zadaniu operatora trójargumentowego <br> przykład: </p>
+<pre><code class="language-php">$a = 13;
+
+$b = $a < 10 ? "Hello" : "Good Bye";
+
+echo $b;</code></pre>
+<p>Zadanie: Skróć poniższy kod</p>
+<pre><code class="language-php">$a = 10;
+$b = 25;
+$c = 15;
+
+if ($a + $b == 35) {
+    $c++;
+}
+
+if ($c <= 15) {
+    $a = $b;
+} else {
+    $a = $c;
+}
+
+echo "$a, $b, $c";</code></pre>
