f0fccb1ec347cdcceb05a71667a04b3f7ef43fe8
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-03-09 09:40:45 +0100

    nagłówki w panelu bocznym

--- PHP/index.php
+++ PHP/index.php
@@ -1,6 +1,7 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/php/head.php'; ?>
 <div class="content">
     <aside>
+        <h3>PHP podstawy</h3>
         <ul>
             <a href="temat.php?title=Wprowadzenie&src=Wprowadzenie"><li>Wprowadzenie</li></a>
             <a href="temat.php?title=Typy danych&src=Typy_danych"><li>Typy danych</li></a>
@@ -8,6 +9,9 @@
             <a href="temat.php?title=Funkcje matematyczne&src=Funkcje_matematyczne"><li>Funkcje Matematyczne</li></a>
             <a href="temat.php?title=Stałe&src=Stale"><li>Stałe w PHP</li></a>
             <a href="temat.php?title=Instrukcje Warunkowe&src=Instrukcje_warunkowe"><li>Instrukcje warunkowe</li></a>
+        </ul>
+        <h3>PHP + MySQL</h3>
+        <ul>
             <a href="temat.php?title=Połączenie z bazą danych&src=polaczenie_z_baza_danych"><li>Połączenie z bazą danych</li></a>
         </ul>
     </aside>
