209626523fbaf6f6ae96fcc8a838e46a7db0dfde
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-03-11 12:10:03 +0100

    hello world test komunikaty

--- PHP/tematy/Wprowadzenie/testy/zadanie1.php
+++ PHP/tematy/Wprowadzenie/testy/zadanie1.php
@@ -1,12 +1,20 @@
 <?php
-$poprawne = strtolower($output) == "hello world!";
-$komunikaty = $poprawne ? ['Zadanie wykonane'] : ['Zadanie niewykonane'];
-return [
-    'poprawne' => $poprawne,
-    'komunikaty' => $komunikaty
-];
+    $poprawne = false;
+    $komunikaty = [];
 
-
+    if ($output == "") {
+        $komunikaty[] = "Nie wypisano danych, upewnij się, że korzystasz z echo";
+    } else {
+        $poprawne = strtolower($output) == "hello world!";
+        if (!$poprawne) {
+            $komunikaty[] = "Wypisano $output zamiast Hello world!";
+        }
+    }
+    $komunikaty[] = $poprawne ? 'Zadanie wykonane' : 'Zadanie niewykonane';
+    return [
+        'poprawne' => $poprawne,
+        'komunikaty' => $komunikaty
+    ];
 ?>
 
 
