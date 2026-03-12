bcd00c71a39e92f1213bc8b6297e1a225b9d84e5
Dominik Możdżeń <dominikmozdzen@gmail.com>
2026-03-12 08:13:56 +0100

    index __DIR__

--- PHP/index.php
+++ PHP/index.php
@@ -1,21 +1,21 @@
 <?php
     $title = $_GET['title'] ?? 'Strona Główna';
     $src = $_GET['src'] ?? null;
-    include $_SERVER['DOCUMENT_ROOT'].'/php/head.php';
+    include __DIR__ . '\\head.php';
     if ($src) {
-        include $_SERVER['DOCUMENT_ROOT'].'/php/navbox.php';
+        include __DIR__ . '\\navbox.php';
     }
 ?>
 <div class="content">
 <?php
-    include $_SERVER['DOCUMENT_ROOT'].'/php/sidebar.php';
+    include __DIR__ . '\\sidebar.php';
     if ($src) {
         echo '<main>';
-        include $_SERVER['DOCUMENT_ROOT'].'/php/temat.php';
+        include __DIR__ . '\\temat.php';
         echo '</main>';
     } else {
-        include $_SERVER['DOCUMENT_ROOT'].'/php/home.php';
+        include __DIR__ . '\\home.php';
     }
 ?>
 </div>
-<?php include $_SERVER['DOCUMENT_ROOT'].'/php/foot.php'; ?>
+<?php include __DIR__ . '\\foot.php'; ?>
